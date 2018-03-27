1a09f1188dbedf9494195eb2babf87b5bb78ecbd
bv <Guest@Epicodus-8A.local>
2018-03-26 20:50:13 -0700

    add prototype tags

--- Desktop/pig-dice/js/scripts.js
+++ Desktop/pig-dice/js/scripts.js
@@ -1,4 +1,22 @@
 //Business Logic
+function playerOne (turn) {
+  this.total1 = 0;
+  this.score = 0;
+  this.maxScore = 100;
+  this.roll1 = 0;
+  this.turn = "yes";
+
+}
+playerOne.prototype.Random = function(){
+  if (this.turn === "no"){
+    console.log("It's not your turn!");
+  } else {
+    this.roll1 = Math.floor(Math.random() * 7);
+    if (this.roll1 === 1){
+      this.score = 0;
+    }
+  }
+}
 
 
 
@@ -9,5 +27,9 @@
 
 //UI Logic
 $(document).ready(function(){
+  $("button").click(function(event){
+    event.preventDefault();
 
+
+  });
 });
